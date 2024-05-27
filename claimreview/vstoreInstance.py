2187adfe39b47566f2a5b2b41e1ece639bb78f51
oceakun <sagardeep1811@hotmail.com>
2024-05-28 01:45:32 +0530

    feat: update RAG system to implement latest version of pinecone

--- claimreview/vstoreInstance.py
+++ claimreview/vstoreInstance.py
@@ -1,17 +1,20 @@
-import pinecone
-from langchain.vectorstores import Pinecone
 import os
 from dotenv import load_dotenv
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore
 
 class vstoreInstance:
-    def __init__(self, pinecone_api_key):
-        self.pinecone_api_key = pinecone_api_key
+    def __init__(self, pinecone_api_key, index_name, pinecone_data_url):
         load_dotenv()  
-        self.index_name = os.getenv("INDEXNAME")  
-        pinecone.init(api_key=pinecone_api_key, environment="gcp-starter")
+        self.pinecone_api_key = pinecone_api_key
+        self.index_name = index_name
+        self.pinecone_data_url = pinecone_data_url
+        self.pc = Pinecone(api_key=pinecone_api_key)
 
     def get_index(self):
-        return pinecone.Index(self.index_name)
+        index = self.pc.Index(host=self.pinecone_data_url)
+        return index
 
     def get_vector_store(self,index, embedder, text_field):
-        return Pinecone(index, embedder.embed_query, text_field)
+        vectorstore = PineconeVectorStore(index, embedder, text_field)
+        return vectorstore
